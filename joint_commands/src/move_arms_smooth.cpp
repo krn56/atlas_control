5286ba4ad9a81d245064c261b6936c441230147d
krn56 <karanpatelkrn@gmail.com>
2018-03-19 17:57:03 -0400

    added another steps to move arms - collision not working

--- joint_commands/src/move_arms_smooth.cpp
+++ joint_commands/src/move_arms_smooth.cpp
@@ -4,7 +4,7 @@
 # include <trajectory_msgs/JointTrajectoryPoint.h>
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "joint_command_animation_node");
+    ros::init(argc, argv, "move_arms_animation_node");
 
 //    ROS_INFO(ros::Time::toSec());
 //    while(ros::Time::toSec() == 0.0){
@@ -38,13 +38,13 @@ int main(int argc, char** argv){
     jt.joint_names.push_back("atlas::l_arm_elx");//16
     jt.joint_names.push_back("atlas::l_arm_ely");//17
     jt.joint_names.push_back("atlas::l_arm_mwx");//18
-    jt.joint_names.push_back("atlas::l_arm_shx");//19
+    jt.joint_names.push_back("atlas::l_arm_shx");//19*
     jt.joint_names.push_back("atlas::l_arm_usy");//20
     jt.joint_names.push_back("atlas::l_arm_uwy");//21
     jt.joint_names.push_back("atlas::r_arm_elx");//22
     jt.joint_names.push_back("atlas::r_arm_ely");//23
     jt.joint_names.push_back("atlas::r_arm_mwx");//24
-    jt.joint_names.push_back("atlas::r_arm_shx");//25
+    jt.joint_names.push_back("atlas::r_arm_shx");//25*
     jt.joint_names.push_back("atlas::r_arm_usy");//27
     jt.joint_names.push_back("atlas::r_arm_uwy");//28
 
@@ -57,6 +57,7 @@ int main(int argc, char** argv){
         static double i = 0;
         double x2 = i;
         double x = 0;
+        double x1 = 2.0;
         p.positions.push_back(x);//0
         p.positions.push_back(x);//1
         p.positions.push_back(x);//2
@@ -64,25 +65,25 @@ int main(int argc, char** argv){
         p.positions.push_back(x);//4
         p.positions.push_back(x);//5
         p.positions.push_back(x);//6
-        p.positions.push_back(x2);//7
+        p.positions.push_back(x1);//7
         p.positions.push_back(x);//8
         p.positions.push_back(x);//9
         p.positions.push_back(x);//10
         p.positions.push_back(x);//11
-        p.positions.push_back(x2);//12
+        p.positions.push_back(x1);//12
         p.positions.push_back(x);//13
         p.positions.push_back(x);//14
         p.positions.push_back(x);//15
         p.positions.push_back(x);//16
         p.positions.push_back(x);//17
         p.positions.push_back(x);//18
-        p.positions.push_back(x);//19
+        p.positions.push_back(x2);//19*
         p.positions.push_back(x);//20
         p.positions.push_back(x);//21
         p.positions.push_back(x);//22
         p.positions.push_back(x);//23
         p.positions.push_back(x);//24
-        p.positions.push_back(x);//25
+        p.positions.push_back(-x2);//25*
         p.positions.push_back(x);//26
         p.positions.push_back(x);//27
 
@@ -91,11 +92,12 @@ int main(int argc, char** argv){
 
         traj_pub.publish(jt);
         ros::spinOnce();
-        i += 0.001;
-        if (i >= 2.0){
+        i += 0.0001;
+        if (i >= 1.39){
             exit(1);
         }
     }
 
     return 0;
 }
+
--- joint_commands/src/move_arms_step3.cpp
+++ joint_commands/src/move_arms_step3.cpp
@@ -4,7 +4,7 @@
 # include <trajectory_msgs/JointTrajectoryPoint.h>
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "joint_command_animation_node");
+    ros::init(argc, argv, "move_arms_step3_node");
 
 //    ROS_INFO(ros::Time::toSec());
 //    while(ros::Time::toSec() == 0.0){
@@ -39,24 +39,25 @@ int main(int argc, char** argv){
     jt.joint_names.push_back("atlas::l_arm_ely");//17
     jt.joint_names.push_back("atlas::l_arm_mwx");//18
     jt.joint_names.push_back("atlas::l_arm_shx");//19
-    jt.joint_names.push_back("atlas::l_arm_usy");//20
+    jt.joint_names.push_back("atlas::l_arm_usy");//20*1.9
     jt.joint_names.push_back("atlas::l_arm_uwy");//21
     jt.joint_names.push_back("atlas::r_arm_elx");//22
     jt.joint_names.push_back("atlas::r_arm_ely");//23
     jt.joint_names.push_back("atlas::r_arm_mwx");//24
     jt.joint_names.push_back("atlas::r_arm_shx");//25
-    jt.joint_names.push_back("atlas::r_arm_usy");//27
-    jt.joint_names.push_back("atlas::r_arm_uwy");//28
+    jt.joint_names.push_back("atlas::r_arm_usy");//26*-1.9
+    jt.joint_names.push_back("atlas::r_arm_uwy");//27
 
     int n = 1200;
     float dt = 0.01;
     float rps = 0.05;
-
-   while(ros::ok()){
+    static double i = 0;
+    ros::Rate loop_rate(1000);
+    while(ros::ok()){
         trajectory_msgs::JointTrajectoryPoint p;
-        static double i = 0;
-        double x2 = i;
+        double x1 = i;
         double x = 0;
+    //        double x1 = 2.0;
         p.positions.push_back(x);//0
         p.positions.push_back(x);//1
         p.positions.push_back(x);//2
@@ -64,38 +65,40 @@ int main(int argc, char** argv){
         p.positions.push_back(x);//4
         p.positions.push_back(x);//5
         p.positions.push_back(x);//6
-        p.positions.push_back(x2);//7
+        p.positions.push_back(2.0);//7
         p.positions.push_back(x);//8
         p.positions.push_back(x);//9
         p.positions.push_back(x);//10
         p.positions.push_back(x);//11
-        p.positions.push_back(x2);//12
+        p.positions.push_back(2.0);//12
         p.positions.push_back(x);//13
         p.positions.push_back(x);//14
         p.positions.push_back(x);//15
         p.positions.push_back(x);//16
         p.positions.push_back(x);//17
         p.positions.push_back(x);//18
-        p.positions.push_back(x);//19
-        p.positions.push_back(x);//20
+        p.positions.push_back(1.39);//19
+        p.positions.push_back(x1);//20*
         p.positions.push_back(x);//21
         p.positions.push_back(x);//22
         p.positions.push_back(x);//23
         p.positions.push_back(x);//24
-        p.positions.push_back(x);//25
-        p.positions.push_back(x);//26
+        p.positions.push_back(-1.39);//25
+        p.positions.push_back(x1);//26*
         p.positions.push_back(x);//27
 
         jt.points.push_back(p);
-        ROS_INFO("test: angles[%f][%f %f]",i,x,x2 );
+        ROS_INFO("test: angles[%f][%f %f]",i,x,x1 );
 
         traj_pub.publish(jt);
         ros::spinOnce();
-        i += 0.001;
-        if (i >= 2.0){
+        loop_rate.sleep();
+        i -= 0.001;
+        if (i <= -1.3){
             exit(1);
         }
     }
 
     return 0;
-}
+    }
+
